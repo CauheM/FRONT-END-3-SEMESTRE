261ee60369c31593cbd4cd6f5d94ec48931baefc
CAUHE SANCHES MATHEUS <54440848800@info.local>
2026-04-22 16:41:11 -0300

    atividade realizada

---
+++ imc-javascript-main/imc-javascript-main/js/script.js
@@ -0,0 +1,40 @@
+function calcular(){
+    //alert("Função calcular rolando");
+    const nome = document.getElementById("nome").value;
+    const altura = parseFloat(document.getElementById("altura").value);
+    const peso = parseFloat(document.getElementById("peso").value);
+    
+    if(nome.trim().length == 0 || isNaN(altura) || isNaN(peso)){
+        alert("PREENCHA OS CAMPOS PARÇA");
+        return false;
+    }  
+    
+}
+
+function calcularImc(altura, peso){
+  return IMC = peso / (altura * altura);
+}
+
+function gerarTextoImc(IMC){
+   if(IMC < 16){
+    return "Magreza Grave";
+   } else if (IMC < 17){
+    return "Magreza moderada";
+   } else if (IMC < 18.5){
+    return "Magreza Leve";
+   } else if (IMC < 25){
+    return "Saudavel";
+   } else if (IMC < 30){
+    return "Sobrepeso";
+   } else if (IMC < 35){
+    return "Obesidade Grau I";
+   } else if (IMC < 40){
+    return "Obesidade Grau II";
+   } else if (IMC > 40){
+    return "Obesidade Grau III";
+   } else if (IMC > 70){
+    return "papo de undaia";
+   } else {
+    return "peso invalido";
+   }
+}
