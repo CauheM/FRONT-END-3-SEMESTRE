1059a45a1558136222e6dacf86c23f244e5ee355
CAUHE SANCHES MATHEUS <54440848800@info.local>
2026-04-24 15:31:08 -0300

    atividade realizada

--- imc-javascript-main/imc-javascript-main/js/script.js
+++ imc-javascript-main/imc-javascript-main/js/script.js
@@ -1,49 +1,127 @@
-function calcular(){
-    //alert("Função calcular rolando");
-    const nome = document.getElementById("nome").value;
-    const altura = parseFloat(document.getElementById("altura").value);
-    const peso = parseFloat(document.getElementById("peso").value);
+  function calcular(){
+      //alert("Função calcular rolando");
+      const nome = document.getElementById("nome").value;
+      const altura = parseFloat(document.getElementById("altura").value);
+      const peso = parseFloat(document.getElementById("peso").value);
+      
+      if(nome.trim().length == 0 || isNaN(altura) || isNaN(peso)){
+          alert("PREENCHA OS CAMPOS PARÇA");
+          return false;
+      }  
+      
+      const IMC = calcularImc(altura, peso);
+      const TextoSituacao = gerarTextoImc(IMC);
+
+    let cadastroAPI = {
+    Nome : nome,
+    Altura : altura,
+    Peso : peso,
+    Imc : IMC,
+    TextoSituacao : TextoSituacao
+    }
+
+    const cadastrado = cadastrarNaApi(cadastroAPI);
     
-    if(nome.trim().length == 0 || isNaN(altura) || isNaN(peso)){
-        alert("PREENCHA OS CAMPOS PARÇA");
-        return false;
-    }  
+
+  if (cadastrado) {
+      const cadastro = document.getElementById("cadastro");
     
-    const IMC = calcularImc(altura, peso);
-    const TextoSituacao = gerarTextoImc(IMC);
+      cadastro.innerHTML += `
+      <tr>
+      <td>${nome}</td>
+      <td>${altura}</td>
+      <td>${peso}</td>
+      <td>${IMC.toFixed(2)}</td>
+      <td>${TextoSituacao}</td>
+      </tr>
+      `;
 
-    console.log(nome);
-    console.log(altura);
-    console.log(peso);
-    console.log(IMC);
-    console.log(TextoSituacao);
+      document.getElementById("nome").value = "";
+      document.getElementById("altura").value = "";
+      document.getElementById("peso").value = " ";
 
-}
+      alert(`${nome} foi cadastrado com sucesso `);
 
-function calcularImc(altura, peso){
-  return IMC = peso / (altura * altura);
-}
+  } else {
+    alert("não foi possivel cadastrar");
+  }
+  }
 
-function gerarTextoImc(IMC){
-   if(IMC < 16){
-    return "Magreza Grave";
-   } else if (IMC < 17){
-    return "Magreza moderada";
-   } else if (IMC < 18.5){
-    return "Magreza Leve";
-   } else if (IMC < 25){
-    return "Saudavel";
-   } else if (IMC < 30){
-    return "Sobrepeso";
-   } else if (IMC < 35){
-    return "Obesidade Grau I";
-   } else if (IMC < 40){
-    return "Obesidade Grau II";
-   } else if (IMC > 40){
-    return "Obesidade Grau III";
-   } else if (IMC > 70){
-    return "papo de undaia";
-   } else {
-    return "peso invalido";
-   }
-}
+  async function cadastrarNaApi(objIMC){
+  try {
+
+      const resposta = await fetch ("http://localhost:3000/CadastroIMC", {
+      method : "POST",
+      body : JSON.stringify(objIMC),
+      headers : {
+        "Content-Type" : "application-json; charset=UTF=8"
+      }
+      });
+
+      return true;
+
+  } catch (error) {
+
+      console.log(error);
+      return false;
+
+  }
+
+  }
+
+
+  function calcularImc(altura, peso){
+    return IMC = peso / (altura * altura);
+  }
+
+  function gerarTextoImc(IMC){  
+    if(IMC < 16){
+      return "Magreza Grave";
+    } else if (IMC < 17){
+      return "Magreza moderada";
+    } else if (IMC < 18.5){
+      return "Magreza Leve";
+    } else if (IMC < 25){
+      return "Saudavel";
+    } else if (IMC < 30){
+      return "Sobrepeso";
+    } else if (IMC < 35){
+      return "Obesidade Grau I";
+    } else if (IMC < 40){
+      return "Obesidade Grau II";
+    } else if (IMC > 40){
+      return "Obesidade Grau III";
+    } else {
+      return "peso invalido";
+    }
+  } 
+    
+  async function BuscarIMCs(){
+
+    try {
+
+    const retorno = await fetch ("http://localhost:3000/CadastroIMC");
+    const dados = await retorno.json();
+
+    console.log(dados); //dados do cadastro
+
+    const cadastro = document.getElementById("cadastro");
+
+    for (let i = 0; i < dados.length; i++){
+      cadastro.innerHTML += `
+        <tr>
+          <td>${dados[i].Nome}</td>
+          <td>${dados[i].Altura}</td>
+          <td>${dados[i].Peso}</td>
+          <td>${dados[i].Imc.toFixed(2)}</td>
+          <td>${dados[i].TextoSituacao}</td>
+        </tr>
+      `;
+    }
+
+    } catch (error) {
+
+      console.log(error); 
+
+    }
+  }
