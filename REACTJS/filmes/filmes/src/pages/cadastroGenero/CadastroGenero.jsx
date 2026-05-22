fc381afef65bae9e9237e554c56e9931d7eebc75
CAUHE SANCHES MATHEUS <54440848800@info.local>
2026-05-22 16:43:17 -0300

    atividade realizada

--- REACTJS/filmes/filmes/src/pages/cadastroGenero/CadastroGenero.jsx
+++ REACTJS/filmes/filmes/src/pages/cadastroGenero/CadastroGenero.jsx
@@ -1,12 +1,117 @@
 import "./CadastroGenero.css";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
+import Cadastro from "../../components/cadastro/Cadastro";
+import Lista from "../../components/lista/Lista";
+import { useEffect, useState } from "react";
+import api from "../../Services/services";
 
 const CadastroGenero = () => { 
+
+    const [valor, setValor] = useState("")
+    const [listaGeneros, SetlistaGeneros] = useState([])
+
+
+    const getGeneros = async () => {
+    try {
+        const retornoAPI = await api.get("/Genero")
+
+        SetlistaGeneros(retornoAPI.data)
+    } catch (error) {
+        alert("problema com a api")
+        console.log(error)
+    }
+    }
+
+    useEffect(() => {
+    getGeneros()
+    }, [])
+
+    const objCadastros = {
+        idgenero : crypto.randomUUID(),
+        nome : valor
+     }
+
+    const cadastrarGenero = async (e) => {
+     e.preventDefault()
+
+     if(valor.trim().length == 0){
+        alert("preenche isso caramba")
+     }
+
+
+     try {
+        const retornoAPI = await api.post("/Genero", objCadastros)
+        alert("cadastrado com sucesso")
+        LimparFormulario()
+        getGeneros()
+     } catch (error) {  
+        alert("deu ruim aqui")
+        console.log(error)
+     }
+    }
+    
+    const excluirGenero = async (e) => {
+
+    try {
+    const retornoAPI = await api.delete(`/Genero/${e.idgenero}`)
+    alert("deletado com sucesso")
+    getGeneros()
+    } catch (error) {
+        alert("deu problema")
+        console.log(error)
+    }
+
+    }
+    
+
+    const editarGenero = () => {
+        alert("função em desenvolvimento")
+    }
+
+    const LimparFormulario = () => {
+        setValor("")
+    }
+
+
     return (
         <>
             <Header />
-            <h1>Cadastro de Gêneros</h1>
+            <main>
+                <Cadastro
+                    //Define o título que será exibido no formulário
+                    tituloCadastro="Cadastro de Gênero"
+                    // esconde o select de genero
+                    visibilidade="none"
+                    // Define o texto que aparece dentro do campo de input
+                    placeholder="gênero"
+                    // ----------------------------------------------------
+                    // Propriedades voltada ao cadastro:
+
+                    //Função que será chamada ao enviar o formulário (onSubmit)
+                    funcCadastro={cadastrarGenero}
+                    //Valor atual do campo de texto
+                    valor={valor}
+                    //Função que atualiza o estado do valor no componente pai sempre que o usuário digita no campo
+                    setValor={setValor}
+
+                    
+                />
+                 <Lista
+                    tituloLista="Lista de Gêneros"
+                    visibilidade="none"
+
+                    //Chama o método para validar:
+                    lista={listaGeneros}
+                    //Identifica o tipo de lista:
+                    tipoLista="genero"
+
+
+                    funcExcluir = {excluirGenero}
+                    funcEditar = {editarGenero}
+                />
+
+            </main>
             <Footer />
         </>
      );
--- REACTJS/filmes/filmes/src/pages/login/Login.jsx
+++ REACTJS/filmes/filmes/src/pages/login/Login.jsx
@@ -1,8 +1,18 @@
 import "./Login.css";
+import { Link } from "react-router-dom";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
 
 const Login = () => { 
     return (
+        <>
+        <Header/>
         <h1>Login</h1>
+        <Link to="/generos">Gêneros</Link>
+        <Link to="/filmes">Filmes</Link>
+        <Footer/>
+        </>
+
      );
 }
 
